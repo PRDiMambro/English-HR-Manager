d602d18d6f2ecf796b7976260c171e45989527a1
Paulo Ricardo Moura Di Mambro <prdimambro@gmail.com>
2021-08-23 22:41:23 -0300

    Test commit

---
+++ English-HR-Manager/Classes-BankHolidays.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace English_HR_Manager
+{
+    public class Holiday
+    {
+        [JsonProperty(PropertyName = "england-and-wales")]
+        public Bank EnglandAndWales { get; set; }
+
+        [JsonProperty(PropertyName = "scotland")]
+        public Bank Scotland { get; set; }
+
+        [JsonProperty(PropertyName = "northern-ireland")]
+        public Bank NorthernIreland { get; set; }
+    }
+    public class Bank
+    {
+        [JsonProperty(PropertyName = "division")]
+        public string Division { get; set; }
+
+        [JsonProperty(PropertyName = "events")]
+        public List<Event> Events { get; set; }
+
+    }
+    public class Event
+    {
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "date")]
+        public DateTime? EventDate { get; set; }
+
+        [JsonProperty(PropertyName = "notes")]
+        public string Notes { get; set; }
+
+        [JsonProperty(PropertyName = "bunting")]
+        public bool? Bunting { get; set; }
+    }
+}
---
+++ English-HR-Manager/Function-BankHolidays.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace English_HR_Manager
+{
+    public static class Function_BankHolidays
+    {
+        [FunctionName("BankHolidays")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous,
+                         "get",
+                         Route = "bank-holidays/{year:int?}")]
+            HttpRequest req,
+
+            int? year)
+        {
+            string responseMessage = null;
+            Holiday holidays = new Holiday();
+
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString("https://www.gov.uk/bank-holidays.json");
+                holidays = JsonConvert.DeserializeObject<Holiday>(json);
+            }
+
+            string csv = String.Join(Environment.NewLine,
+                                     holidays.EnglandAndWales.Events
+                                             .Where(x => x.EventDate.GetValueOrDefault().Year == year)
+                                             .OrderByDescending(x => x.EventDate.GetValueOrDefault())
+                                             .Select(x => $"{x.Title}," +
+                                                          $"{x.EventDate.ToString()}," +
+                                                          $"{x.Notes}," +
+                                                          $"{x.Bunting.ToString()}"
+                                                    ).ToArray()
+                                     );
+
+            toBlob(, "bankholidays", "BankHolidays.csv", csv);
+
+            responseMessage = csv;
+            return await Task.FromResult(new OkObjectResult(responseMessage));
+        }
+        public static void toBlob(string storageConnectionString,
+                       string containerString,
+                       string blobName,
+                       string text)
+        {
+        var storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+        var client = storageAccount.CreateCloudBlobClient();
+        var container = client.GetContainerReference(containerString);
+        var blob = container.GetBlockBlobReference(blobName);
+        using (CloudBlobStream x = blob.OpenWriteAsync().Result)
+        {
+            x.Write(System.Text.Encoding.Default.GetBytes(text.ToString() + "\n"));
+            x.Flush();
+            x.Close();
+        }
+    }
+
+
+    }
+
+    
+}
